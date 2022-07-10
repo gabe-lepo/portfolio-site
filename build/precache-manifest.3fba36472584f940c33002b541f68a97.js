1c602e7de6844ac1bd2ee789015d1b71b2f6ee4b
gabe-gardyn <gabriel@gardyn.tech>
2022-07-10 13:49:10 -0400

    rebuilt app with ts template and bulma

---
+++ build/precache-manifest.3fba36472584f940c33002b541f68a97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eba5e2b6e8d2b0c104d9",
+    "url": "/static/css/main.032c6501.chunk.css"
+  },
+  {
+    "revision": "eba5e2b6e8d2b0c104d9",
+    "url": "/static/js/main.7a1f5601.chunk.js"
+  },
+  {
+    "revision": "bb1f08fe37d3c65590eb",
+    "url": "/static/js/runtime~main.e015284a.js"
+  },
+  {
+    "revision": "316cc844a04bf0ef5f9d",
+    "url": "/static/js/2.926424c2.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1b8e425ec4df0f191aa394e2196d24d4",
+    "url": "/index.html"
+  }
+];
