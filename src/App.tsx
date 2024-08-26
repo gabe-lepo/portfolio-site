845a508d60596e94874a34bb34f4f3dc019bee5d
gabe-gardyn <gabriel@gardyn.tech>
2024-08-26 11:29:17 -0400

    reactive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import CarStuff from "./pages/CarStuff";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import NavBar from "./components/NavBar"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import CarStuff from "./pages/CarStuff"
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/carstuff" element={<CarStuff />} />
-        </Routes>
-      </Router>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/carstuff" element={<CarStuff />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,23 @@
-export default function NavBar() {
-   return(
-      <div className="navbar" role="navigation" aria-label="main navigation">
-         <div className="navbar-brand">
-            <a className="navbar-item title" href="/">Home</a>
-         </div>
+import { useLocation } from "react-router-dom"
 
-         <div className="navbar-menu">
-            <div className="navbar-start">
-               <a className="navbar-item" href="/projects">Projects</a>
-               <a className="navbar-item" href="/carstuff">Car Stuff</a>
-            </div>
+export default function NavBar() {
+   const locationPath = useLocation().pathname
+
+   return(
+      <div className="navbar is-fixed-top is-dark" role="navigation" aria-label="main navigation">
+         <div className="navbar-brand">
+            <a
+               className={`navbar-item ${locationPath === "/" ? "has-background-grey-dark has-text-weight-bold" : ""}`}
+               href="/"
+            >Home</a>
+            <a
+               className={`navbar-item ${locationPath === "/projects" ? "has-background-grey-dark has-text-weight-bold" : ""}`}
+               href="/projects"
+            >Projects</a>
+            <a
+               className={`navbar-item ${locationPath === "/carstuff" ? "has-background-grey-dark has-text-weight-bold" : ""}`}
+               href="/carstuff"
+            >Car Stuff</a>
          </div>
       </div>
    )
