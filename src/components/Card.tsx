18f1107e941b3e787fac9895771099dfaff09f16
gabe-gardyn <gabriel@gardyn.tech>
2024-08-26 13:03:39 -0400

    added card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+interface CardProps {
+   imgSrc?: string
+   title: string
+   subtitle?: string
+   children?: React.ReactNode
+}
+
+export default function Card(props: CardProps) {
+   return(
+      <div className="card">
+         <div className="card-content">
+            <div className="media">
+               <div className="media-left">
+                  <figure className="image is-64x64">
+                     <img
+                        src={props.imgSrc}
+                        alt="Missing source"
+                     />
+                  </figure>
+               </div>
+               <div className="media-content">
+                  <p className="title is-4">{props.title}</p>
+                  <p className="subtitle is-6">{props.subtitle}</p>
+               </div>
+            </div>
+            <div className="content">
+               {props.children}
+            </div>
+         </div>
+      </div>
+   )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import Card from "../components/Card";
 import PageHero from "../components/PageHero";
 
 export default function Home() {
    return(
       <>
-         <PageHero title="Home" subtitle="Base page" classes="is-link"/>
+         <PageHero title="Home" subtitle="Resume and info about me" classes="is-link"/>
+         <Card title="Gabriel Lepoutre" subtitle="A subtitle!" imgSrc="/favicon.ico">
+            <p>This is some card content</p>
+         </Card>
       </>
    );
 };
