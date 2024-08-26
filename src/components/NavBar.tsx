115f28c9b37e0841704b1f64af193ec736544c93
gabe-gardyn <gabriel@gardyn.tech>
2024-08-25 23:28:17 -0400

    back to bulma, fixed manifest, installed missing dependency

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,16 @@
-import { Navbar, Container, Nav } from "react-bootstrap"
-
 export default function NavBar() {
    return(
-      <Navbar expand="lg" className="bg-body-tertiary">
-         <Container>
-            <Navbar.Brand href="/">Home</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-               <Nav className="me-auto">
-                  <Nav.Link href="projects">Projects</Nav.Link>
-                  <Nav.Link href="carstuff">Car Stuff</Nav.Link>
-               </Nav>
-            </Navbar.Collapse>
-         </Container>
-      </Navbar>
+      <div className="navbar" role="navigation" aria-label="main navigation">
+         <div className="navbar-brand">
+            <a className="navbar-item title" href="/">Home</a>
+         </div>
+
+         <div className="navbar-menu">
+            <div className="navbar-start">
+               <a className="navbar-item" href="/projects">Projects</a>
+               <a className="navbar-item" href="/carstuff">Car Stuff</a>
+            </div>
+         </div>
+      </div>
    )
 }
