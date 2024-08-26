851d50d37d114c5fc9e8bfc5e687d16eb46d20d3
gabe-gardyn <gabriel@gardyn.tech>
2024-08-26 13:30:20 -0400

    basic grid pattern

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,50 @@ export default function Home() {
    return(
       <>
          <PageHero title="Home" subtitle="Resume and info about me" classes="is-link"/>
-         <Card title="Gabriel Lepoutre" subtitle="A subtitle!" imgSrc="/favicon.ico">
-            <p>This is some card content</p>
-         </Card>
+         <div className="section">
+            <div className="grid is-col-min-15 is-gap-1">
+               <div className="cell">
+                  <Card title="Gabriel Lepoutre" subtitle="A subtitle!" imgSrc="/favicon.ico">
+                     <p>This is some card content</p>
+                  </Card>
+               </div>
+               <div className="cell">
+                  <Card title="Temp card" subtitle="subtitle" imgSrc="/favicon.ico">
+                     <p>card content</p>
+                  </Card>   
+               </div>
+               <div className="cell">
+                  <Card title="Temp card" subtitle="subtitle" imgSrc="/favicon.ico">
+                     <p>card content</p>
+                  </Card>   
+               </div>
+               <div className="cell">
+                  <Card title="Temp card" subtitle="subtitle" imgSrc="/favicon.ico">
+                     <p>card content</p>
+                  </Card>   
+               </div>
+               <div className="cell">
+                  <Card title="Temp card" subtitle="subtitle" imgSrc="/favicon.ico">
+                     <p>card content</p>
+                  </Card>   
+               </div>
+               <div className="cell">
+                  <Card title="Temp card" subtitle="subtitle" imgSrc="/favicon.ico">
+                     <p>card content</p>
+                  </Card>   
+               </div>
+               <div className="cell">
+                  <Card title="Temp card" subtitle="subtitle" imgSrc="/favicon.ico">
+                     <p>card content</p>
+                  </Card>   
+               </div>
+               <div className="cell">
+                  <Card title="Temp card" subtitle="subtitle" imgSrc="/favicon.ico">
+                     <p>card content</p>
+                  </Card>   
+               </div>
+            </div>
+         </div>
       </>
    );
 };
